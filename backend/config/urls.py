3cbc05b6375a6b032b0ae1d8e86f3a6ff6a4aca4
ssyucfa <payk1230@bk.ru>
2021-09-08 22:41:38 +0300

    initial project files

---
+++ backend/config/urls.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from .auto_docs_urls import urlpatterns as swagger
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+
+# other urls
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
+]
+
+
+# our urls
+urlpatterns += [
+
+]
+
+
+# swagger urls
+urlpatterns += swagger
+
+
+
