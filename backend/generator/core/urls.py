3c4c9154e9da628c5c32efa64c02fbda888fbbd2
ssyucfa <payk1230@bk.ru>
2021-09-11 12:59:13 +0300

    before review

--- backend/generator/core/urls.py
+++ backend/generator/core/urls.py
@@ -1,14 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .auto_docs_urls import urlpatterns as swagger
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-
-
-# other urls
-urlpatterns += [
-    path('api-auth/', include('rest_framework.urls')),
 ]
