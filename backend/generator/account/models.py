4c2d604da4a40c964ff1d3c4890e5f9550317a48
ssyucfa <payk1230@bk.ru>
2021-09-12 18:46:20 +0300

    created app and model

---
+++ backend/generator/account/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Profile(models.Model):
+    full_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=150)
+    number = models.CharField(15)
+    about_me = models.TextField()
+    website = models.URLField()
+    experience = models.PositiveSmallIntegerField(default=0)
+
+    def __str__(self):
+        return self.full_name
