9cc620dadc9abc3fbd49760145b9447cbfd9755b
ssyucfa <payk1230@bk.ru>
2021-09-14 21:54:49 +0300

    created app and model

--- backend/generator/profile/models.py
+++ backend/generator/profile/models.py
@@ -1,7 +1,9 @@
+from django.conf import settings
 from django.db import models
 
 
 class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None)
     full_name = models.CharField(max_length=255)
     email = models.EmailField(max_length=150)
     number = models.CharField(max_length=15)
