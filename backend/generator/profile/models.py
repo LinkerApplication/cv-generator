34fcaeec92319195210e56aaf74752b482b9e5db
ssyucfa <payk1230@bk.ru>
2021-09-14 22:34:01 +0300

    created app and model

--- backend/generator/profile/models.py
+++ backend/generator/profile/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None)
-    full_name = models.CharField(max_length=255)
-    email = models.EmailField(max_length=150)
-    number = models.CharField(max_length=15)
     about_me = models.TextField(blank=True)
+    email = models.EmailField(max_length=150)
+    full_name = models.CharField(max_length=255)
+    number = models.CharField(max_length=15)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None)
     website = models.URLField(blank=True)
 
     def __str__(self):
