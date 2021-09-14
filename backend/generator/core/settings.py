325e687cc55adfd0f23bf960848d9f3caed6629c
ssyucfa <88593210+ssyucfa@users.noreply.github.com>
2021-09-14 22:42:08 +0300

    created app and model (#21)
    * created app and model
    
    * created app and model
    
    * created app and model
    
    * created app and model
    
    * created app and model
    
    * created app and model
    
    * changed gitignore
    
    * created app and model
    
    * created app and model
    
    * created app and model

--- backend/generator/core/settings.py
+++ backend/generator/core/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'rest_framework',
 
     # our apps
-
+    'profile',
 ]
 
 MIDDLEWARE = [
---
+++ backend/generator/profile/models.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.db import models
+
+
+class Profile(models.Model):
+    about_me = models.TextField(blank=True)
+    email = models.EmailField(max_length=150)
+    full_name = models.CharField(max_length=255)
+    number = models.CharField(max_length=15)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None)
+    website = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.full_name
