a660c2af3fb4032b1cf94ee70f9c0ff31a656d89
ssyucfa <88593210+ssyucfa@users.noreply.github.com>
2021-09-20 22:18:37 +0300

    created model works place (#26)
    * created model works place
    
    * created model works place
    
    * created model works place
    
    * created model works place
    
    * created model works place

--- backend/generator/profile/models.py
+++ backend/generator/profile/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.postgres.fields import DateRangeField
 from django.db import models
 
 
@@ -12,3 +13,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.full_name
+
+
+class Experience(models.Model):
+    description = models.TextField(max_length=2000)
+    experience = DateRangeField()
+    employer = models.CharField(max_length=255)
+    position = models.CharField(max_length=150)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='experiences')
+
+    def __str__(self):
+        return f'{self.profile.full_name}: {self.employer}'
