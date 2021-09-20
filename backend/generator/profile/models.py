6c26756af84b0a145e6cdce54c6c48d5bcd8dd84
ssyucfa <payk1230@bk.ru>
2021-09-20 18:36:12 +0300

    created model works place

--- backend/generator/profile/models.py
+++ backend/generator/profile/models.py
@@ -9,6 +9,12 @@ class Profile(models.Model):
     number = models.CharField(max_length=15)
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None)
     website = models.URLField(blank=True)
+    works = models.ManyToManyField('WorksPlace', blank=True)
 
     def __str__(self):
         return self.full_name
+
+
+class WorksPlace(models.Model):
+    profession = models.CharField(max_length=150)
+    experience = models.PositiveSmallIntegerField(max_length=2)
