493fbc9ebc26a6015db852eb725db2a8b86de962
ssyucfa <payk1230@bk.ru>
2021-09-20 21:19:56 +0300

    created model works place

--- backend/generator/profile/models.py
+++ backend/generator/profile/models.py
@@ -16,11 +16,12 @@ class Profile(models.Model):
 
 class Experience(models.Model):
     description = models.TextField(max_length=2000)
-    experience = models.PositiveSmallIntegerField()
-    firm = models.CharField(max_length=255)
+    _from = models.DateField()
+    to = models.DateField()
+    employer = models.CharField(max_length=255)
     position = models.CharField(max_length=150)
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='experiences')
     used_technology = models.TextField(blank=True)
 
     def __str__(self):
-        return f'{self.profile.full_name}: {self.firm}'
+        return f'{self.profile.full_name}: {self.employer}'
