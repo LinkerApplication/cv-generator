73a7f3f2c5459a665125deedade008330e7dd3ee
ssyucfa <payk1230@bk.ru>
2021-09-27 22:51:41 +0300

    created new baseuser

--- backend/generator/user/managers.py
+++ backend/generator/user/managers.py
@@ -9,7 +9,7 @@ class UserManager(BaseUserManager):
     Custom user model manager where email is the unique identifiers
     for authentication instead of usernames.
     """
-    def _create_user(self, email: EmailField, password: CharField, **extra_fields) -> settings.AUTH_USER_MODEL:
+    def _create_user(self, email: str, password: str, **extra_fields) -> settings.AUTH_USER_MODEL:
         """
         Create and save a User with the given email and password.
         """
@@ -21,7 +21,7 @@ class UserManager(BaseUserManager):
         user.save()
         return user
 
-    def create_superuser(self, email: EmailField, password: CharField, **extra_fields):
+    def create_superuser(self, email: str, password: str, **extra_fields):
         """
         Create and save a SuperUser with the given email and password.
         """
