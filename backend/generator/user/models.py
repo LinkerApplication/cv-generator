0eb21b73263528da0f6a47538c3c8b4c2b3870c4
ssyucfa <payk1230@bk.ru>
2021-09-27 22:36:09 +0300

    created new baseuser

--- backend/generator/user/models.py
+++ backend/generator/user/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from .managers import UserManager
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
@@ -8,6 +9,8 @@ class User(AbstractUser):
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+    
+    objects = UserManager()
     
     def __str__(self):
         return self.email    
