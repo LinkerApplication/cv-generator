dca38bddcccc14355233251937f452a3b36cd912
ssyucfa <payk1230@bk.ru>
2021-10-03 19:41:25 +0300

    added test to creating users

---
+++ backend/generator/user/tests/test_creating_users.py
@@ -0,0 +1,47 @@
+import pytest
+from django.contrib.auth import get_user_model
+
+
+@pytest.mark.django_db
+def test_create_user():
+    User = get_user_model()
+    user = User.objects.create_user(email='normal@user.com', password='foo')
+    assert user.email == 'normal@user.com'
+    assert user.is_active
+    assert not user.is_staff
+    assert not user.is_superuser
+
+    try:
+        # username is None for the AbstractUser option
+        # username does not exist for the AbstractBaseUser option
+        assert user.username is None
+    except AttributeError:
+        pass
+
+    with pytest.raises(TypeError):
+        User.objects.create_user()
+    with pytest.raises(TypeError):
+        User.objects.create_user(email='')
+    with pytest.raises(TypeError):
+        User.objects.create_user(email='', password="foo")
+
+
+@pytest.mark.django_db
+def test_create_superuser():
+    User = get_user_model()
+    admin_user = User.objects.create_superuser(email='super@user.com', password='foo')
+    assert admin_user.email == 'super@user.com'
+    assert admin_user.is_active
+    assert admin_user.is_staff
+    assert admin_user.is_superuser
+    try:
+        # username is None for the AbstractUser option
+        # username does not exist for the AbstractBaseUser option
+        assert admin_user.username is None
+    except AttributeError:
+        pass
+
+    with pytest.raises(ValueError):
+        User.objects.create_superuser(
+            email='super@user.com', password='foo', is_superuser=False
+        )
