ea7526876b673b7f881185ad6b43e6e244c3e5b1
ssyucfa <payk1230@bk.ru>
2021-10-05 19:29:48 +0300

    update tests to creating users

--- backend/generator/user/tests/test_creating_users.py
+++ backend/generator/user/tests/test_creating_users.py
@@ -11,19 +11,33 @@ def test_create_user():
     assert not user.is_staff
     assert not user.is_superuser
 
+
+@pytest.mark.django_db
+def test_negative_create_user():
+    User = get_user_model()
+    with pytest.raises(TypeError):
+        user = User.objects.create_user()
+        if user.email is None or user.password is None:
+            raise TypeError
+    with pytest.raises(TypeError):
+        user = User.objects.create_user(email='')
+        if user.password is None:
+            raise TypeError
+    with pytest.raises(ValueError):
+        user = User.objects.create_user(email='', password="foo")
+
+
+@pytest.mark.django_db
+def test_not_exist_username():
+    User = get_user_model()
+    user = User.objects.create_user(email='normal@user.com', password='foo')
+
     try:
         # username is None for the AbstractUser option
         # username does not exist for the AbstractBaseUser option
         assert user.username is None
     except AttributeError:
         pass
-
-    with pytest.raises(TypeError):
-        User.objects.create_user()
-    with pytest.raises(TypeError):
-        User.objects.create_user(email='')
-    with pytest.raises(ValueError):
-        User.objects.create_user(email='', password="foo")
 
 
 @pytest.mark.django_db
@@ -34,14 +48,27 @@ def test_create_superuser():
     assert admin_user.is_active
     assert admin_user.is_staff
     assert admin_user.is_superuser
+
+
+@pytest.mark.django_db
+def test_negative_create_superuser():
+    User = get_user_model()
+    admin_user = User.objects.create_superuser(email='super@user.com', password='foo')
+
+    with pytest.raises(ValueError):
+        User.objects.create_superuser(
+            email='super@user.com', password='foo', is_superuser=False
+        )
+
+
+@pytest.mark.django_db
+def test_not_exist_admin_username():
+    User = get_user_model()
+    admin_user = User.objects.create_superuser(email='super@user.com', password='foo')
+
     try:
         # username is None for the AbstractUser option
         # username does not exist for the AbstractBaseUser option
         assert admin_user.username is None
     except AttributeError:
         pass
-
-    with pytest.raises(ValueError):
-        User.objects.create_superuser(
-            email='super@user.com', password='foo', is_superuser=False
-        )
