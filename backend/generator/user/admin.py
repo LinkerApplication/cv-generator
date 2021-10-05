37c1f7b902ac139276e832a26ad29cdf51e95281
ssyucfa <payk1230@bk.ru>
2021-10-05 20:02:16 +0300

    register user to admin

--- backend/generator/user/admin.py
+++ backend/generator/user/admin.py
@@ -2,8 +2,19 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from .models import User
+from .forms import CustomUserCreationForm
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    add_form = CustomUserCreationForm
+
+    def get_form(self, request, obj=None, **kwargs):
+        """
+        Use special form during user creation
+        """
+        defaults = {}
+        if obj is None:
+            defaults['form'] = self.add_form
+        defaults.update(kwargs)
+        return super().get_form(request, obj, **defaults)
