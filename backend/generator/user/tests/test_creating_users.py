691540a02a6e60f21a4ad2a76dfc48b64782239c
Artem Jasan <58912225+artemjasan@users.noreply.github.com>
2021-10-10 22:54:51 +0200

    Add Github Actions (#38)
    * Add Github Actions
    
    * Update github actions
    
    * Remove unused step
    
    * Separate each checker to the signle job
    
    * Separate each checker to the signle job and add some fitures
    
    * Fix
    
    * ignore black
    
    * ignore black and add path
    
    * ignore black and add path v2
    
    * Fix black step
    
    * Fix cov report No1
    
    * Fix cov report No2
    
    * Fix cov report No3
    
    * Fix cov report No4
    
    * Fix cov report No5

--- backend/generator/user/tests/test_creating_users.py
+++ backend/generator/user/tests/test_creating_users.py
@@ -55,9 +55,7 @@ def test_negative_create_superuser():
     User = get_user_model()
 
     with pytest.raises(ValueError):
-        User.objects.create_superuser(
-            email="super@user.com", password="foo", is_superuser=False
-        )
+        User.objects.create_superuser(email="super@user.com", password="foo", is_superuser=False)
 
 
 @pytest.mark.django_db
