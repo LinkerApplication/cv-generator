24ebf6db0a2c7db2699775ac2267e20a200e63fa
Artem Jasan <58912225+artemjasan@users.noreply.github.com>
2021-10-29 12:37:39 +0200

    JWT authentication (#39)
    * Update
    
    * Configurate settings for JWT auth
    
    * Update path
    
    * fix urls path

--- backend/generator/core/urls.py
+++ backend/generator/core/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+api_urlpatterns = [
+    path("auth/", include("user.urls")),  # TODO: rewrite to users
+]
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include(api_urlpatterns)),
 ]
